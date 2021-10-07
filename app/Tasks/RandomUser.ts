1586d58fbd2b204b33d7b489dd0da28350512552
bhayron <bhayronklivilan@gmail.com>
2021-10-06 23:29:58 -0300

    creation of migration, model and listing of users

--- app/Tasks/RandomUser.ts
+++ app/Tasks/RandomUser.ts
@@ -1,5 +1,6 @@
 import Logger from '@ioc:Adonis/Core/Logger'
 import { BaseTask } from 'adonis5-scheduler/build'
+import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class TaskToCheckSomething extends BaseTask {
   public static get schedule() {
@@ -11,6 +12,10 @@ export default class TaskToCheckSomething extends BaseTask {
   }
 
   public async handle() {
+    const users = await Database.query().from('users').select('*')
+
+    console.log(users)
+
     const ms = new Date().getTime()
     Logger.info('handle start', ms)
     await new Promise((resolve) => setTimeout(resolve, 3000))
